8b20fa9f91c44e6c42b3f466be516a0ac6bc7690
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2023-08-02 23:57:12 +0200

    Add data directory
    Add OS user defined data directory to manage user instances and instead of storing the `WebContext` beneath the executable store it in that directory

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use directories::ProjectDirs;
 use open::that;
 use wry::application::event::{Event, StartCause, WindowEvent};
 use wry::application::event_loop::{ControlFlow, EventLoop};
 use wry::application::window::WindowBuilder;
-use wry::webview::WebViewBuilder;
+use wry::webview::{WebContext, WebViewBuilder};
 use wry::Result;
 
 fn main() -> Result<()> {
     let event_loop = EventLoop::new();
+
+    let data_directory = ProjectDirs::from("", "kiracoding", env!("CARGO_PKG_NAME"))
+        .map(|project_dirs| Some(project_dirs.config_dir().to_path_buf()))
+        .unwrap_or(None);
+
+    let web_context = &mut WebContext::new(data_directory);
 
     let _webview = WebViewBuilder::new(
         WindowBuilder::new()
@@ -16,6 +23,7 @@ fn main() -> Result<()> {
             .build(&event_loop)?,
     )?
     .with_url("https://discord.com/app")?
+    .with_web_context(web_context)
     .with_new_window_req_handler(|url| {
         let _ = that(url);
         false
