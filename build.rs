71085f0f77c3f778210b33f472d0b0c96755cbcb
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-02-17 15:13:41 +0100

    Add build dep `winres`

--- build.rs
+++ build.rs
@@ -5,13 +5,31 @@ use swc_common::source_map::SourceMap;
 use swc_common::GLOBALS;
 
 fn main() {
+    #[cfg(all(target_os = "windows", not(debug_assertions)))]
+    windows_res();
+
+    compile_js();
+}
+
+#[cfg(all(target_os = "windows", not(debug_assertions)))]
+fn windows_res() {
+    use winres::WindowsResource;
+
+    println!("cargo:rerun-if-changed=assets/volt.ico");
+
+    WindowsResource::new()
+        .set_icon("./assets/volt.ico")
+        .set("InternalName", "Volt")
+        .compile()
+        .unwrap();
+}
+
+fn compile_js() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/init.js");
 
     let cm = Arc::<SourceMap>::default();
-
     let compiler = Compiler::new(cm.clone());
-
     let output = GLOBALS
         .set(&Default::default(), || {
             try_with_handler(cm.clone(), Default::default(), |handler| {
