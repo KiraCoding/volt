d99bac3278cbbffc06496343be33703ad856244d
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-05-31 14:33:16 +0200

    Update types

--- src/init.ts
+++ src/init.ts
@@ -3,21 +3,23 @@ const disallowedURLs = /https:\/\/discord\.com\/api\/v\d+\/science/;
 const originalOpen = XMLHttpRequest.prototype.open;
 
 Object.defineProperty(XMLHttpRequest.prototype, "open", {
-  value: function (method, url) {
+  value: function (method: string, url: string) {
     if (method === "POST" && disallowedURLs.test(url)) {
       throw new Error(`XMLHttpRequest to "${url}" with POST method is blocked`);
     }
 
-    return originalOpen.apply(this, arguments);
+    return originalOpen.apply(this, arguments as any);
   },
 });
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const styleElement = document.createElement("style");
-  styleElement.innerHTML = `
-  [class|=listItem]:has([data-list-item-id=guildsnav___app-download-button]),
-  [class|=listItem]:has(+ [class|=listItem] [data-list-item-id=guildsnav___app-download-button]) {
-      display: none;
-  }`;
+
+  const rules = [
+    "[class|=listItem]:has([data-list-item-id=guildsnav___app-download-button]) { display: none; }",
+    "[class|=listItem]:has(+ [class|=listItem] [data-list-item-id=guildsnav___app-download-button]) { display: none; }",
+  ];
+
+  styleElement.innerHTML = rules.join("\n");
   document.head.appendChild(styleElement);
 });
